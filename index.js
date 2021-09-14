26430332acf7ba998f69ac112811bb1ca561dce1
John Shivers <johnshivers3@gmail.com>
2021-09-14 18:15:52 -0400

    create css layout

---
+++ index.js
@@ -0,0 +1,11 @@
+Window.addEventListener('DomContentLoaded', ()=> {
+  const appLink = document.getElementById('app-link')
+  const blogLink = document.getElementById('blog-link')
+  const storeLink = document.getElementById('store-link')
+  const gameLink = document.getElementById('game-link')
+  const noteLink = document.getElementById('note-link')
+
+
+
+
+})
